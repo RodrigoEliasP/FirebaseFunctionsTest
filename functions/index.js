1a2ca06d25f9f2b2833d7b6f278aec973225c190
RodrigoEliasP <rodrigo.e2003@gmail.com>
2021-06-21 11:14:21 -0300

    read messages logic implementation

--- functions/index.js
+++ functions/index.js
@@ -10,18 +10,32 @@ const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+
 exports.addMessage = functions.https.onRequest(async (req, res) => {
     const original = req.query.text;
     const writeResult = 
-    await admin.firestore().collection('messages')
-    .add({original: original});
+    await admin.firestore().collection('messages').add({original: original});
     res.json({result:`Message with Id: ${writeResult.id} added.`});
 });
 
-exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
+exports.makeUppercase = functions.firestore.document('messages/{documentId}')
     .onCreate((snap, context) =>{
         const original = snap.data().original;
         functions.logger.log('Uppercasing', context.params.documentId, original);
         const uppercase = original.toUpperCase();
         return snap.ref.set({uppercase}, {merge:true});
     });
+
+exports.readMessages = functions.https.onRequest(
+    (req,res) => {
+        admin.firestore().collection('messages').get().then(docs => {
+            const result = docs.docs.map( doc => {
+                return {
+                    id: doc.id,
+                    data: doc.data()
+                }
+            })
+            res.status(200).json(result);
+        })
+    }
+);
